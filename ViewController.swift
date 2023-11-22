588603284539df9a18ffaa9559f4e694dda5f5c3
shaik sadaf patel <shaiksadafpatel786@gmail.com>
2023-11-22 23:02:51 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by shaik sadaf patel on 09/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
